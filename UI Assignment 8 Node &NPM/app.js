0ddb2e8e281044a0a8d7f0e62b4a5bbf1949c842
meta-yash-porwal <yash.porwal@metacube.com>
2021-04-06 13:36:46 +0530

    Displaying index.html content after running node server (app.js file)

--- UI Assignment 8 Node &NPM/app.js
+++ UI Assignment 8 Node &NPM/app.js
@@ -1,9 +1,18 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to NodeJS');
-});
-
-server.listen(3000);
+const http = require('http'); 
+const fs = require('fs'); 
+// Server configuration 
+const hostname = '127.0.0.1'; 
+const port = 3000; 
+// Render page 
+fs.readFile('./index.html', function (err, html) { 
+ if (err) { 
+ 	throw err;  
+ }  
+ const server = http.createServer(function(request, response) {  
+ 	response.writeHeader(200, {"Content-Type": "text/html"});  
+ 	response.write(html);  
+ 	response.end();  
+ }).listen(port, hostname, () => { 
+	  console.log(`Server running at http://${hostname}:${port}/`); 
+    }); 
+}); 
